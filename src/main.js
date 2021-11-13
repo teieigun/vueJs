10cbe8f37832f706890fa0c57f205011071caad4
teieigun <teieigun@gmail.com>
2021-11-13 09:42:25 +0900

    vueインストール手順html作成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css' //追加
+import 'bootstrap-vue/dist/bootstrap-vue.css' //追加
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
